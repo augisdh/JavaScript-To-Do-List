f9d47bff0d1ed5f3fcb40712dd71526d4eda6fe9
Augustas Skidzevičius <34551243+augisdh@users.noreply.github.com>
2018-04-10 00:50:02 +0300

    withoutComments

--- javascript/todo.js
+++ javascript/todo.js
@@ -1,33 +1,26 @@
 "use strict";
 
-//-- Pirmas To Do sąrašas --//
-
-//-- Susiradau klases ir priskyriau naujus kintamuosius --/
 let toDoList = document.querySelector(".toDo");
 let inputEl = document.querySelector(".my-input");
 const dataEl = document.querySelector(".data");
 const btnEl = document.querySelector(".my-btn");
 
-//stiliai h1 ir pirmo <li>//
 toDoList.style.color = "#000";
 
-//-- Event kuris ideda info i nauja <li> --//
 btnEl.addEventListener("click", function(){
-    let inputValue = inputEl.value; // Nuskaito ivesta reiksme
+    let inputValue = inputEl.value;
         if (inputValue === ""){
-            alert("Privaloma užpildyti laukelį -nauja užduotis-"); // Jeigu nieko neivesta praso ivesti... 
+            alert("Privaloma užpildyti laukelį -nauja užduotis-"); 
         } else {
-            const newLi = document.createElement("li"); // Sukuria naujus <li> tag
-            dataEl.appendChild(newLi);  // Ideda naujus <li> po pirmo <li saraso su klase>
-            newLi.append(inputValue);   // Ideda value i naujus <li> tag
+            const newLi = document.createElement("li"); 
+            dataEl.appendChild(newLi);  
+            newLi.append(inputValue);   
 
-            //stiliai nauju <li>//
             newLi.style.paddingBottom = "10px";
             newLi.style.color = "#3f3f3f";
 
-            inputEl.value = ""; // Isvalo input'a
+            inputEl.value = "";
 
-            // Istrinimo mygtukas, istrina visus naujus <li> tag
             const btnClear = document.querySelector(".clear-btn");
             btnClear.addEventListener("click", function(){
                 newLi.remove();
@@ -35,48 +28,43 @@ btnEl.addEventListener("click", function(){
         }
 });
 
-//-- Naujo sąrašo sukūrimas mygtukas --//
 let createList = document.querySelector(".naujas-btn");
 
-//-- Sukuria nauja sarasa --//
 createList.addEventListener("click", function(){
-    let newHeadingName = prompt("Įveskite pavadinimą"); // Duoda ivesti pavadinima saraso
-        if (newHeadingName === "" || newHeadingName === null){ // Tikrina ar yra ivestas pavadinimas
-            alert("Privaloma įvesti pavadinimą");   // Jei pavadinomo nera = alert
+    let newHeadingName = prompt("Įveskite pavadinimą");
+        if (newHeadingName === "" || newHeadingName === null){
+            alert("Privaloma įvesti pavadinimą");   
         } else {
-    let newDiv = document.createElement("div"); // Sukuria nauja <div> tag kurio viduje bus naujas sarasas
+    let newDiv = document.createElement("div"); 
     newDiv.className = "praktika";
-    createList.before(newDiv); // Ikelia <div> tag virs "naujas sarasas" <button>
+    createList.before(newDiv); 
 
-    let newHeading = document.createElement("h1"); // Sukuria nauja <h1> 
-    newDiv.appendChild(newHeading); // Ikelia <h1> i nauja <div> tag
-    newHeading.innerHTML = newHeadingName;  // i <h1> paraso ivesta pavadinima
+    let newHeading = document.createElement("h1"); 
+    newDiv.appendChild(newHeading); 
+    newHeading.innerHTML = newHeadingName; 
 
-    const newUl = document.createElement("ul"); // Sukuriu nauja <ul>
-    newHeading.after(newUl); // Idedu <ul> po <h1>
+    const newUl = document.createElement("ul"); 
+    newHeading.after(newUl); 
 
-    let newInput = document.createElement("input"); // Sukuriu nauja input 
+    let newInput = document.createElement("input"); 
     newInput.placeholder = "naujas"; 
-    newUl.after(newInput); // Idedu input po <ul>
+    newUl.after(newInput); 
 
-    const newDivBtn = document.createElement("div"); // sukuriu div 
-    newInput.after(newDivBtn); // Idedu <div> po input
+    const newDivBtn = document.createElement("div");
+    newInput.after(newDivBtn); 
 
-    // Sukuriu mygtuka (prideti) ji idedu i nauja <div>
     const newBtnAdd = document.createElement("button");
     newBtnAdd.type = "button";
     newBtnAdd.className = "my-btnNew";
     newBtnAdd.append("Pridėti");
     newDivBtn.appendChild(newBtnAdd);
 
-    // Sukuriu mygtuka (istrinti) kuris yra idetas po (prideti) mygtuko
     const newBtnRemove = document.createElement("button");
     newBtnRemove.type = "button";
     newBtnRemove.className = "clear-btnNew";
     newBtnRemove.append("Išvalyti");
     newBtnAdd.after(newBtnRemove);
 
-    // Naujas event kai paspaudziamas (Prideti) mygtukas
     newBtnAdd.addEventListener("click", function(){
         let newInputValue = newInput.value;
             if(newInputValue === ""){
@@ -87,11 +75,9 @@ createList.addEventListener("click", function(){
                 newLi.append(newInputValue);
                 newInput.value = "";
 
-                // Stiliai <li> //
                 newLi.style.paddingBottom = "10px";
                 newLi.style.color = "#3f3f3f";
                 
-                // Naujas event kai paspaudziamas (Istrinti) mygtukas
                 newBtnRemove.addEventListener("click", function(){
                     newLi.remove();
                 });
